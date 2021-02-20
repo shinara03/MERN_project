8d60a885c6d94ffc709cbea270b6b6b881d73031
shinara03 <shinara03@gmail.com>
2021-02-20 12:27:35 -0800

    finish setup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://shinara03:AI9ChicU5tdV4LhI@cluster0.2eehl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+            //Make sure this is your own unique string
+}
