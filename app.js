3ffe377d96b3941b8f0f3d794e90de616240d758
shinara03 <shinara03@gmail.com>
2021-02-20 18:46:06 -0800

    finish registration

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -22,10 +22,11 @@ app.get("/", (req, res) => {
   user.save()
   res.send("Hello, World!")
 });
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
